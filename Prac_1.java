153fd2e1c14889bf92ae9a4f9a59ea0bca84af25
Karan Kamble <kbkamble18@gmail.com>
2025-11-14 14:30:14 +0530

    Add files via upload

---
+++ Prac_1.java
@@ -0,0 +1,31 @@
+//import java.util.*;
+public class Prac_1 {
+      public static void main(String[] args){    
+    /*System.out.println("Enter number of elements to be displayed");
+    //Scanner sc = new Scanner(System.in);
+   // int size = sc.nextInt();
+    int no1[] = new int[3];
+    for(int i=0;i<3;i++){
+    System.out.println(no1[i]);
+    */
+    int no2[] = {23,45,65,78,26,98};
+    System.out.println(no2.length);
+    /*
+    for(int i =0;i<no2.length;i++){
+    if(no2[i]==78){
+    System.out.println(no2[i]+" number found at "+ i+ " position.");
+    }
+    */
+    //}
+    
+    
+    
+  }
+  class LL{
+  class Node{
+  String data;
+  
+  }
+  }
+}
+
